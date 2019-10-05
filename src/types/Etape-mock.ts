a9fc8b35ccc4d6f411517d0bd830cce6cee12bdf
Olivier Neu <olivierneu.one@gmail.com>
2019-10-05 16:37:24 -0400

    Modification du mock d'étape

--- src/types/Etape-mock.ts
+++ src/types/Etape-mock.ts
@@ -5,18 +5,21 @@ export const ETAPES: Etape[] = [
         numeroEtape: 2,
         etapeCourante: false,
         nomEtape: 'Etape 2',
-        url: '/web/symposium2019/page2'
+        url: '/web/symposium2019/page2',
+        visited: false
     },
     {
         numeroEtape: 1,
         etapeCourante: true,
         nomEtape: 'Etape 1',
-        url: '/web/symposium2019/page1'
+        url: '/web/symposium2019/page1',
+        visited: false
     },
     {
         numeroEtape: 3,
         etapeCourante: false,
         nomEtape: 'Etape 3',
-        url: '/web/symposium2019/page3'
+        url: '/web/symposium2019/page3',
+        visited: false
     }
 ];
